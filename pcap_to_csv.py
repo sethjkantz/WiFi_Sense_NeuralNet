87118a7e0061743e83e4a414a7cd785180d09c55
Seth Kantz <sethjkantz@gmail.com>
2026-04-04 22:09:06 -0400

    Updated to run over multiple files.

--- pcap_to_csv.py
+++ pcap_to_csv.py
@@ -1,17 +1,27 @@
 from csiread import Nexmon
 import numpy as np
 import pandas as pd
+from pathlib import Path
 
 FNAME = "walk_test_1038.pcap"
 OUTNAME = FNAME + ".csv"
 
-csidata = Nexmon(FNAME, chip="43455c0",bw=80)
-csidata.read()
+PCAP_DIR = Path("pcap_files")
+
+for pcap_file in PCAP_DIR.glob("*.pcap"):
+    FNAME = str(pcap_file)
+    OUTNAME = str(pcap_file.with_suffix(".csv"))
+    print(f"Processing: {FNAME}...")
+
+    csidata = Nexmon(FNAME, chip="43455c0",bw=80)
+    csidata.read()
 
 
-# Convert to magnitude
-# @TODO should we also include complex in CSV?
-csi_mag = np.abs(csidata.csi)
+    # Convert to magnitude
+    # @TODO should we also include complex in CSV?
+    csi_mag = np.abs(csidata.csi)
 
-df = pd.DataFrame(csi_mag.reshape(csi_mag.shape[0], -1))
-df.to_csv(OUTNAME, index=False)
+    df = pd.DataFrame(csi_mag.reshape(csi_mag.shape[0], -1))
+    df.to_csv(OUTNAME, index=False)
+    print(f"Done")
+
